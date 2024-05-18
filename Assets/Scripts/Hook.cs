e18ac7cdbc44e64e1c997e51a8abde5b5afbec8d
crvanya <34192519+crvanya@users.noreply.github.com>
2024-05-18 23:52:30 +0300

    0.0.2

---
+++ Assets/Scripts/Hook.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Hook : MonoBehaviour
+{
+    private Camera _camera;
+    private Vector3 _mousePosition;
+    private Vector3 _currentPosition;
+    private DistanceJoint2D _distanceJoint;
+    private LineRenderer _lineRenderer;
+    private bool _cheak;
+    void Start()
+    {
+        _camera = Camera.main;
+        _distanceJoint = GetComponent<DistanceJoint2D>();
+        _lineRenderer = GetComponent<LineRenderer>();
+        _distanceJoint.enabled = false;
+        _lineRenderer.positionCount = 0;
+        _cheak = true;
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        MouseMove();
+        RaycastHit2D rayHit = Physics2D.Raycast(_camera.transform.position, _mousePosition, Mathf.Infinity);
+        if (Input.GetMouseButton(0) && _cheak && rayHit)
+        {
+            _distanceJoint.enabled = true;
+            _distanceJoint.connectedAnchor = _mousePosition;
+            _lineRenderer.positionCount = 2;
+            _currentPosition = _mousePosition;
+            _cheak = false;
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            _distanceJoint.enabled = false;
+            _cheak = true;
+            _lineRenderer.positionCount = 0;
+        }
+    }
+    private void DrawLine()
+    {
+        if(_lineRenderer.positionCount <= 0) return;
+        _lineRenderer.SetPosition(0, transform.position);
+        _lineRenderer.SetPosition(1, _currentPosition);
+    }
+    private void MouseMove()
+    {
+        _mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
